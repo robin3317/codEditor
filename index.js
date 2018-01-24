368091542cf423f27c4625ab7a8c14ca5a3e2f37
robin3317 <abdurrahmanrobin47@gmail.com>
2018-01-25 04:42:28 +0600

    Cheers ! Done

--- index.js
+++ index.js
@@ -26,7 +26,24 @@ $(".panel").height($(window).height() - $("#header").height() -20);
 $(".panel").width(($(window).width()/2) - 15);
 
 //updating output
-$("iframe").contents().find("html").html($("#htmlPanel").val());
+updateOutput();
 $("textarea").on("change keyup paste", function() {
-  $("iframe").contents().find("html").html($("#htmlPanel").val());
+  updateOutput();
 })
+
+//update output function
+function updateOutput() {
+  $("iframe").contents().find("html").html(
+    "<html>"+
+      "<head>"+
+        "<style type='text/css'>"+
+          $("#cssPanel").val()+
+        "</style>"+
+        "</head>"+
+        "<body>"+
+          $("#htmlPanel").val()+
+        "</body>"+
+    "</html>"
+  );
+  document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+}
